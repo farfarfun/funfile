8bc26e4dc35ff487c4d3f49dcdad4c3fa1500d98
farfun <1007530194@qq.com>
2024-11-15 17:48:42 +0800

    add

--- src/funfile/funos.py
+++ src/funfile/funos.py
@@ -1,12 +1,12 @@
 import os
 import shutil
 
+
 def makedirs(path):
     if not os.path.exists(path):
-        os.makedirs(path)
+        os.makedirs(path, exist_ok=True)
 
 
- 
 def delete(path):
     if os.path.exists(path):
-        shutil.rmtree(path)
+        shutil.rmtree(path, ignore_errors=True)
