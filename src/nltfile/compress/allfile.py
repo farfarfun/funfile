ee58458e48f3a89a6edddde2c60a5e4c797f8f93
farfarfun <farfarfun@qq.com>
2026-08-03 10:50:19 +0800

    Remove IDE configs, refactor archive logic, update ZipFile impl

--- src/nltfile/compress/allfile.py
+++ src/nltfile/compress/allfile.py
@@ -1,12 +1,19 @@
+import os
+import shutil
+
 from nltfile.compress import tarfile, zipfile
 
 _TAR_EXTENSIONS = (".tar", ".tar.gz", ".tgz", ".tar.bz2", ".tar.xz", ".txz")
 
 
 def extractall(archive_path: str, path: str = "."):
-    if archive_path.endswith(".zip"):
+    archive_path = os.fspath(archive_path)
+    lower_path = archive_path.lower()
+    if lower_path.endswith(".zip"):
         with zipfile.ZipFile(archive_path) as zf:
             zf.extractall(path=path)
-    elif archive_path.endswith(_TAR_EXTENSIONS):
-        with tarfile.TarFile(archive_path) as tf:
+    elif lower_path.endswith(_TAR_EXTENSIONS):
+        with tarfile.open(archive_path, "r:*") as tf:
             tf.extractall(path=path)
+    else:
+        raise shutil.ReadError(f"unsupported archive format: {archive_path}")
