e21a70506dcf23ce1975456f4218b88aed33499b
farfarfun <farfarfun@qq.com>
2026-08-28 17:26:48 +0800

    refactor: remove deprecated nltfile compatibility package and bump version
    Old package remains resolvable on PyPI forever at its final published
    version; this stops future releases under the nltfile name.
    
    part of farfarfun/todo-list#300

--- extbuild/nltfile-compat/src/nltfile/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Compatibility namespace for the renamed funfile package."""
-
-import warnings
-
-warnings.warn("nltfile was renamed to funfile", DeprecationWarning, stacklevel=2)
-
-from funfile import *  # noqa: E402,F401,F403
-from funfile import __all__, __path__  # noqa: E402,F401
